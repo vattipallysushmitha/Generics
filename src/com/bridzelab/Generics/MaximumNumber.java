62a2ed24e51a017cefd968afad8a0e4f5ec023d0
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-27 14:51:14 +0530

    [SUSHMITHA]ADDED:added the maximum of 3 strings to find

--- src/com/bridzelab/Generics/MaximumNumber.java
+++ src/com/bridzelab/Generics/MaximumNumber.java
@@ -61,15 +61,40 @@ public class MaximumNumber
 	            }
 	        }
 	    }
-	
+	//Method to compare String
+    public static String compareTo(String str1,String str2,String str3) {
+        if(str1.compareTo(str2) > 0)
+        {
+            if(str1.compareTo(str3) > 0)
+            {
+                return str1;
+            }
+            else 
+            {
+                return str3;
+            }
+        } 
+        else 
+        {
+            if(str2.compareTo(str3) > 0)
+            {
+                return str2;
+            }
+            else 
+            {
+                return str3;
+            }
+        }
+    }
 	public static void main(String[] args)
 	{
-		//welcome from user
+		//welcome  from  user to print the message
 		System.out.println("************************");
 		System.out.println("Welcome to JAVA Generics");
 		System.out.println("************************");
 		System.out.println("Maximum from 3 Integer Object is : "+compareTo(876,120,290));
 		System.out.println("Maximum from 3 Float Object is : "+compareTo(56.2f,38.5f,29.0f));
+        System.out.println("Maximum from 3 String Object : "+compareTo("Apple","Peach","Banana"));
 
 	}
 
