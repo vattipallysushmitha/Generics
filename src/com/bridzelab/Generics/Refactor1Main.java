ca73fbdf71de6db0111dd16ad4d3881de17b8cbe
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-08-28 11:40:44 +0530

    [SUSHMITHA]ADDED:finding maximum of 3 variables by generic type

---
+++ src/com/bridzelab/Generics/Refactor1Main.java
@@ -0,0 +1,23 @@
+package com.bridzelab.Generics;
+
+public class Refactor1Main
+{
+
+	public static void main(String[] args) 
+    {
+		//welcome  from  user to print the message
+		System.out.println("**********************************************************");
+		System.out.println("Welcome to JAVA Generics developed by Vattipally Sushmitha");
+		System.out.println("**********************************************************");
+        //Creating Generic Float Object for Float Comparison
+    	Refactor1 integerComparison = new Refactor1();
+        System.out.println("Maximum from 3 Integer Object : "+integerComparison.compareTo(5460,18475,830));
+        //Creating Generic Float Object for Float Comparison
+        Refactor1 floatComparison = new Refactor1();
+        System.out.println("Maximum from 3 Float Object : "+floatComparison.compareTo(876.0F,12.0F,6570.0F));
+        //Creating Generic String Object for String Comparison
+        Refactor1 stringComparison = new Refactor1();
+        System.out.println("Maximum from 3 String Object : "+stringComparison.compareTo("Apple","avacado","promagranate"));
+    }
+
+}
